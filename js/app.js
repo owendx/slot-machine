f3b7dce49ea0849562033bb3a2cc2e296a66491f
oluga <stprodigy@live.com>
2022-07-27 11:19:30 -0700

    code cleanup, replaced all instances of 'var' with 'let'

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 /*----- constants -----*/
 
+//settings for the slot machine
+const numberOfReels = 3; // used to set the number of reels on the slot machine, default is 3
 
 
 /*----- app's state (variables) -----*/
@@ -16,32 +18,45 @@
 
 
 /*----- functions -----*/
+// *** this function CONTROLS THE SPEED of the REEL SPIN ANIMATION and is the core of the slot machine ***
+// add class .nudgeReel to a random div with class 'slot-machine-window'
+function nudgeReel() {
+    let i;
+    let slotMachineWindows = document.getElementsByClassName("slot-machine-window");
+    // store the classes '.nudgeReelLow', '.nudgeReelMed', and '.nudgeReelHigh' in an array
+    let nudgeReelSpeeds = ["nudgeReelLow", "nudgeReelMed", "nudgeReelHigh"];
+    // assign a random index from nudgeReelSpeeds to a class on each div with class 'slot-machine-window'
+    for (i = 0; i < slotMachineWindows.length; i++) {
+        slotMachineWindows[i].classList.add(nudgeReelSpeeds[Math.floor(Math.random() * nudgeReelSpeeds.length)]);
+    }
 
+    // on transitionend, run the function shiftDisplays()
+    for (i = 0; i < slotMachineWindows.length; i++) {
+        slotMachineWindows[i].addEventListener("transitionend", shiftDisplays);
+    }
+}
 
-//settings for the slot machine
-let numberOfReels = 3;
 
 //create 3 divs with class slot-machine-window and append them to the div with class slot-machine
 function createSlotMachineReels() {
-    var slotMachine = document.getElementById("slot-machine");
-    var slotMachineReel = document.createElement("div");
+    let slotMachine = document.getElementById("slot-machine");
+    let slotMachineReel = document.createElement("div");
     slotMachineReel.className = "slot-machine-reel";
     slotMachine.appendChild(slotMachineReel);
 }
 
 // append a div with a class named 'window' to each div with class 'slot-machine-reel'
 function createSlotMachineWindows() {
-    var slotMachineReels = document.getElementsByClassName("slot-machine-reel");
-    for (var i = 0; i < slotMachineReels.length; i++) {
-        var slotMachineWindow = document.createElement("div");
+    let slotMachineReels = document.getElementsByClassName("slot-machine-reel");
+    for (let i = 0; i < slotMachineReels.length; i++) {
+        let slotMachineWindow = document.createElement("div");
         slotMachineWindow.className = "slot-machine-window";
         slotMachineReels[i].appendChild(slotMachineWindow);
     }
 }
 
-
 // run createSlotMachineReels function for the numberOfReels times
-for (var i = 0; i < numberOfReels; i++) {
+for (let i = 0; i < numberOfReels; i++) {
     createSlotMachineReels();
 }
 
@@ -50,22 +65,22 @@ createSlotMachineWindows();
 
 // append a div with class 'displaySet' to each div with class 'slot-machine-window'
 function createDisplaySets() {
-    var slotMachineWindows = document.getElementsByClassName("slot-machine-window");
-    for (var i = 0; i < slotMachineWindows.length; i++) {
-        var displaySet = document.createElement("div");
+    let slotMachineWindows = document.getElementsByClassName("slot-machine-window");
+    for (let i = 0; i < slotMachineWindows.length; i++) {
+        let displaySet = document.createElement("div");
         displaySet.className = "displaySet";
         slotMachineWindows[i].appendChild(displaySet);
     }
 }
 
-// attatch the display sets to the windows
+// attach the display sets to the windows
 createDisplaySets();
 
 // append a div with class 'displaySetDuplicate' to each div with class 'slot-machine-window'
 function createDisplaySetsDuplicates() {
-    var slotMachineWindows = document.getElementsByClassName("slot-machine-window");
-    for (var i = 0; i < slotMachineWindows.length; i++) {
-        var displaySetDuplicate = document.createElement("div");
+    let slotMachineWindows = document.getElementsByClassName("slot-machine-window");
+    for (let i = 0; i < slotMachineWindows.length; i++) {
+        let displaySetDuplicate = document.createElement("div");
         displaySetDuplicate.className = "displaySetDuplicate";
         slotMachineWindows[i].appendChild(displaySetDuplicate);
     }
@@ -76,12 +91,11 @@ createDisplaySetsDuplicates();
 
 // create 9 divs with class 'display' and append them to each div with class 'displaySet'
 function createDisplays() {
-    var displaySets = document.getElementsByClassName("displaySet");
-    for (var i = 0; i < displaySets.length; i++) {
-        for (var j = 0; j < 9; j++) {
-            var display = document.createElement("div");
+    let displaySets = document.getElementsByClassName("displaySet");
+    for (let i = 0; i < displaySets.length; i++) {
+        for (let j = 0; j < 9; j++) {
+            let display = document.createElement("div");
             display.className = `display display${j+1}`;
-            // display.className = `display${j+1}`;
             displaySets[i].appendChild(display);
         }
     }
@@ -91,12 +105,11 @@ createDisplays()
 
 // create 9 divs with class 'display' and append them to each div with class 'displaySetDuplicate'
 function createDisplaysDuplicates() {
-    var displaySets = document.getElementsByClassName("displaySetDuplicate");
-    for (var i = 0; i < displaySets.length; i++) {
-        for (var j = 0; j < 9; j++) {
-            var display = document.createElement("div");
+    let displaySets = document.getElementsByClassName("displaySetDuplicate");
+    for (let i = 0; i < displaySets.length; i++) {
+        for (let j = 0; j < 9; j++) {
+            let display = document.createElement("div");
             display.className = `display display${j+1}`;
-            // display.id = `display${j+1}`;
             displaySets[i].appendChild(display);
         }
     }
@@ -106,8 +119,8 @@ createDisplaysDuplicates()
 
 // generate an array with the numbers 1-9
 function generateRandomNumbers() {
-    var randomNumbers = [];
-    for (var i = 0; i < 9; i++) {
+    let randomNumbers = [];
+    for (let i = 0; i < 9; i++) {
         randomNumbers.push(i + 1);
     }
     return randomNumbers;
@@ -115,7 +128,7 @@ function generateRandomNumbers() {
 
 // shuffle the array
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     // While there remain elements to shuffle...
     while (0 !== currentIndex) {
@@ -133,76 +146,20 @@ function shuffle(array) {
     return array;
 }
 
-console.log(shuffle(generateRandomNumbers()));
-
-
 // store all the divs with class 'slot-machine-window to a variable
 let slotMachineWindows = document.getElementsByClassName("slot-machine-window");
 
 // loop through slotMachineWindows and add a random number to each div under slotMachineWindows
 function addRandomNumbers() {
-    for (var i = 0; i < slotMachineWindows.length; i++) {
+    for (let i = 0; i < slotMachineWindows.length; i++) {
         let setOfNumbers = shuffle(generateRandomNumbers());
-        // let setOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // used for unshuffled number testing
-        // count the number of divs with class 'display' in each div with class 'slot-machine-window'
         let numberOfDisplays = slotMachineWindows[i].getElementsByClassName("display").length;
         // assign each value from setOfNumbers to each div with class 'display'
-        for (var j = 0; j < numberOfDisplays; j++) {
+        for (let j = 0; j < numberOfDisplays; j++) {
             slotMachineWindows[i].getElementsByClassName("display")[j].innerHTML = setOfNumbers[j % setOfNumbers.length];
         }
     }}
 addRandomNumbers();
-
-
-
-// when the 'spin' button is clicked, the class 'displaySetSpin' is added to each div with class 'displaySet' and
-// the class 'displaySetDuplicateSpin' is added to each div with class 'displaySetDuplicate'
-function spin() {
-    var displaySets = document.getElementsByClassName("displaySet");
-    var displaySetsDuplicate = document.getElementsByClassName("displaySetDuplicate");
-    // add the class 'displaySetSpinHigh' to each div with class 'display' under each div with class 'displaySet'
-    for (var i = 0; i < displaySets.length; i++) {
-        displaySets[i].classList.add("displaySetSpin");
-    }
-    // add the class 'displaySetDuplicateSpinHigh' to each div with class 'display' under each div with class 'displaySetDuplicate'
-    for (var i = 0; i < displaySetsDuplicate.length; i++) {
-        displaySetsDuplicate[i].classList.add("displaySetDuplicateSpin");
-    }
-
-    // on animationend, remove the class 'displaySetSpinHigh' from each div with class 'display' under each div with class 'displaySet'
-    for (var i = 0; i < displaySets.length; i++) {
-        displaySets[i].addEventListener("animationend", function() {
-                this.classList.remove("displaySetSpin");
-            }
-        );
-    }
-    // on animationend, remove the class 'displaySetDuplicateSpinHigh' from each div with class 'display' under each div with class 'displaySetDuplicate'
-    for (var i = 0; i < displaySetsDuplicate.length; i++) {
-        displaySetsDuplicate[i].addEventListener("animationend", function() {
-                this.classList.remove("displaySetDuplicateSpin");
-            }
-        );
-    }
-
-
-}
-
-// add class .nudgeReel to a random div with class 'slot-machine-window'
-function nudgeReel() {
-    var slotMachineWindows = document.getElementsByClassName("slot-machine-window");
-    // store the classes '.nudgeReelLow', '.nudgeReelMedium', and '.nudgeReelHigh' in an array
-    var nudgeReelSpeeds = ["nudgeReelLow", "nudgeReelMed", "nudgeReelHigh"];
-    // assign a random index from nudgeReelSpeeds to a class on each div with class 'slot-machine-window'
-    for (var i = 0; i < slotMachineWindows.length; i++) {
-        slotMachineWindows[i].classList.add(nudgeReelSpeeds[Math.floor(Math.random() * nudgeReelSpeeds.length)]);
-    }
-
-    // on transitionend, run the function shiftDisplays()
-    for (var i = 0; i < slotMachineWindows.length; i++) {
-        slotMachineWindows[i].addEventListener("transitionend", shiftDisplays);
-    }
-}
-
 
 
 function shiftDisplaysBy3(displaySet) {
@@ -259,8 +216,6 @@ function shiftDisplaysBy3(displaySet) {
 
 }
 
-
-
 function shiftDisplaysBy5(displaySet) {
     // remap the innerText of each div with class 'display' to a corresponding div with class 'display' under the div with class 'displaySet'
     // retrieve the innerText of div with class 'display1' under the div with class 'displaySet'
@@ -313,7 +268,6 @@ function shiftDisplaysBy5(displaySet) {
     displaySet.getElementsByClassName("display5")[0].innerText = display1;
 
 }
-
 
 function shiftDisplaysBy7(displaySet) {
     // remap the innerText of each div with class 'display' to a corresponding div with class 'display' under the div with class 'displaySet'
@@ -368,13 +322,11 @@ function shiftDisplaysBy7(displaySet) {
 
 }
 
-
-
 // after a nudgeReel is called, trigger the appropriate shiftDisplaysBy function depending on which
 // nudgeReelSpeed is assigned to the parent div with class 'slot-machine-window'
 function shiftDisplays() {
-    var slotMachineWindows = document.getElementsByClassName("slot-machine-window");
-    for (var i = 0; i < slotMachineWindows.length; i++) {
+    let slotMachineWindows = document.getElementsByClassName("slot-machine-window");
+    for (let i = 0; i < slotMachineWindows.length; i++) {
         if (slotMachineWindows[i].classList.contains("nudgeReelLow")) {
             // retrieve the child div with class 'displaySet' and pass it to shiftDisplaysBy3
             shiftDisplaysBy3(slotMachineWindows[i].getElementsByClassName("displaySet")[0]);
@@ -396,20 +348,20 @@ function shiftDisplays() {
     }
 
 
-// add class .resetPosition to each div with class 'slot-machine-window'
-//     resetPosition();
     removeNudgeReelClasses()
 
 
 }
+
 // add class .resetPosition to each div with class 'slot-machine-window'
 function resetPosition() {
-    var slotMachineWindows = document.getElementsByClassName("slot-machine-window");
-    for (var i = 0; i < slotMachineWindows.length; i++) {
+    let i;
+    let slotMachineWindows = document.getElementsByClassName("slot-machine-window");
+    for (i = 0; i < slotMachineWindows.length; i++) {
         slotMachineWindows[i].classList.add("resetPosition");
     }
     // on transitionend, remove the class 'resetPosition' from each div with class 'slot-machine-window'
-    for (var i = 0; i < slotMachineWindows.length; i++) {
+    for (i = 0; i < slotMachineWindows.length; i++) {
         slotMachineWindows[i].addEventListener("transitionend", function() {
             this.classList.remove("resetPosition");
         })
@@ -417,9 +369,10 @@ function resetPosition() {
 // remove the class 'nudgeReelLow', 'nudgeReelMed', and 'nudgeReelHigh' from each div with class 'slot-machine-window'
 
 }
+
 function removeNudgeReelClasses() {
-    var slotMachineWindows = document.getElementsByClassName("slot-machine-window");
-    for (var i = 0; i < slotMachineWindows.length; i++) {
+    let slotMachineWindows = document.getElementsByClassName("slot-machine-window");
+    for (let i = 0; i < slotMachineWindows.length; i++) {
         slotMachineWindows[i].classList.remove("nudgeReelLow");
         slotMachineWindows[i].classList.remove("nudgeReelMed");
         slotMachineWindows[i].classList.remove("nudgeReelHigh");
@@ -432,38 +385,7 @@ function removeNudgeReelClasses() {
 document.getElementById("spin").addEventListener("click", nudgeReel);
 
 
-// when the button with id 'spinX' is clicked, the class 'spinner' is added to each div with class 'displaySet' and
-function spinnerX() {
-    var displaySets = document.getElementsByClassName("displaySet");
-    var displaySetsDuplicate = document.getElementsByClassName("displaySetDuplicate");
-    // add the class 'spinner' to each div with class 'displaySet'
-    for (var i = 0; i < displaySets.length; i++) {
-        displaySets[i].classList.add("spinner");
-    }
 
-    // add the class 'spinner' to each div with class 'displaySetDuplicate'
-    for (var i = 0; i < displaySetsDuplicate.length; i++) {
-        displaySetsDuplicate[i].classList.add("spinner");
-
-    }
-
-    // on animationend, remove the class 'spinner' from each div with class 'displaySet'
-    for (var i = 0; i < displaySets.length; i++) {
-        displaySets[i].addEventListener("animationend", function() {
-                this.classList.remove("spinner");
-            }
-        );
-    }
-    // on animationend, remove the class 'spinner' from each div with class 'displaySetDuplicate'
-    for (var i = 0; i < displaySetsDuplicate.length; i++) {
-        displaySetsDuplicate[i].addEventListener("animationend", function() {
-                this.classList.remove("spinner");
-            }
-        );
-    }
-
-
-}
 
 
 
